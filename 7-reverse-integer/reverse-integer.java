8a03155b42a014b2a64fb69d1ccec7997960a636
2410991091 <dolisha1091.becse24@chitkara.edu.in>
2026-02-21 12:35:32 +0530

    Time: 1 ms (99.82%) | Memory: 42.5 MB (91.67%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int reverse(int x) {
+        int rev = 0;
+
+        while (x != 0) {
+            int d = x % 10;   // last digit
+            x = x / 10;      // remove last digit
+
+            // check overflow before multiplying
+            if (rev > Integer.MAX_VALUE / 10 || rev < Integer.MIN_VALUE / 10) {
+                return 0;
+            }
+
+            rev = rev * 10 + d;
+        }
+
+        return rev;
+    }
+}
