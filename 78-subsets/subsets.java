811dbc1adb32631c81147f439dba8d0b261f05e3
2410991091 <dolisha1091.becse24@chitkara.edu.in>
2026-03-30 13:33:20 +0530

    Time: 1 ms (88.18%) | Memory: 44 MB (82.72%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,31 +1,22 @@
-import java.util.*;
-
 class Solution {
-    
-    public static void rec(int idx, int[] nums, List<Integer> a, List<List<Integer>> ans) {
-        int n = nums.length;
-
-        // Base case
-        if (idx == n) {
+    public static void rec(int idx,int[] nums,List<Integer> a,List<List<Integer>> ans){
+        int n=nums.length;
+        if(idx==n){
             ans.add(new ArrayList<>(a));
             return;
         }
-
-        // Pick the element
         a.add(nums[idx]);
-        rec(idx + 1, nums, a, ans);
+        rec(idx+1,nums,a,ans);
+        a.remove(a.size()-1);
+        rec(idx+1,nums,a,ans);
 
-        // Backtrack (remove picked element)
-        a.remove(a.size() - 1);
-
-        // Not pick the element
-        rec(idx + 1, nums, a, ans);
     }
-
     public List<List<Integer>> subsets(int[] nums) {
-        List<Integer> a = new ArrayList<>();
-        List<List<Integer>> ans = new ArrayList<>();
-        rec(0, nums, a, ans);
+        List<Integer> a=new ArrayList<>();
+        List<List<Integer>> ans=new ArrayList<>();
+        rec(0,nums,a,ans);
         return ans;
+        
+        
     }
 }
