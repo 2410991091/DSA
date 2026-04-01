5e7b0bad58b098b9718f04aa666f53f95c6a2bf0
2410991091 <dolisha1091.becse24@chitkara.edu.in>
2026-04-01 23:35:36 +0530

    Time: 3 ms (7.98%) | Memory: 45.3 MB (84.89%) - LeetSync

---
+++ 46-permutations/permutations.java
@@ -0,0 +1,25 @@
+class Solution {
+    public void rec(int[] nums,List<Integer> ds,List<List<Integer>> ans,boolean[] freq){
+        if(ds.size()==nums.length){
+            ans.add(new ArrayList<>(ds));
+        }
+        for(int i=0;i<nums.length;i++){
+            if(!freq[i]){
+                freq[i]=true;
+                ds.add(nums[i]);
+                rec(nums,ds,ans,freq);
+                ds.remove(ds.size()-1);
+                freq[i]=false;
+            }
+        }
+
+    }
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> ans=new ArrayList<>();
+        List<Integer> ds =new ArrayList<>();
+        boolean freq[]=new boolean[nums.length];
+        rec(nums,ds,ans,freq);
+        return ans;
+        
+    }
+}
