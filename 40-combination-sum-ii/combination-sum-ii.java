d995f38c5dd48fc005ae20738a21c559cd3209f0
2410991091 <dolisha1091.becse24@chitkara.edu.in>
2026-04-02 13:40:48 +0530

    Time: 6 ms (73.83%) | Memory: 50.7 MB (5.29%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.java
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -1,21 +1,29 @@
 class Solution {
-    public static void rec(int idx,int[] arr,int target, List<Integer> ds,List<List<Integer>> ans){
-        int n=arr.length;
+
+
+
+
+    public static void rec(int idx,int[] arr,int target,List<Integer> ds,List<List<Integer>> ans){
         if(target==0){
             ans.add(new ArrayList<>(ds));
+
         }
-        for(int i=idx;i<n;i++){
+
+        for(int i=idx;i<arr.length;i++){
             if(i>idx && arr[i]==arr[i-1]) continue;
             if(arr[i]>target) break;
             ds.add(arr[i]);
-            rec(i+1,arr,target-arr[i],ds,ans);
-            ds.remove(ds.size()-1);
+
+rec(i+1,arr,target-arr[i],ds,ans);
+
+ds.remove(ds.size()-1);
+
 
         }
     }
     public List<List<Integer>> combinationSum2(int[] arr, int target) {
-        List<List<Integer>> ans=new ArrayList<>();
         List<Integer> ds=new ArrayList<>();
+        List<List<Integer>> ans=new ArrayList<>();
         Arrays.sort(arr);
         rec(0,arr,target,ds,ans);
         return ans;
