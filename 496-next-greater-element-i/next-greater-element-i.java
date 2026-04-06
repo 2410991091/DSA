a8648095bd581617268e2197a8ed1917de5f1539
2410991091 <dolisha1091.becse24@chitkara.edu.in>
2026-04-06 16:46:15 +0530

    Time: 4 ms (74.38%) | Memory: 44.8 MB (82.58%) - LeetSync

---
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int[] ans = new int[nums1.length];
+
+        for (int i = 0; i < nums1.length; i++) {
+            int idx = -1;
+
+            // find element in nums2
+            for (int j = 0; j < nums2.length; j++) {
+                if (nums2[j] == nums1[i]) {
+                    idx = j;
+                    break;
+                }
+            }
+
+            // find next greater element
+            int nge = -1;
+            for (int j = idx + 1; j < nums2.length; j++) {
+                if (nums2[j] > nums1[i]) {
+                    nge = nums2[j];
+                    break;
+                }
+            }
+
+            ans[i] = nge;
+        }
+
+        return ans;
+    }
+}
