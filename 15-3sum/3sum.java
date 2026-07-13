fe406e474e5cee402a8fd799275ca7dada356333
Dolisha Arora <dolisha1091.becse24@chitkara.edu.in>
2026-07-13 12:15:59 +0530

    Time: 36 ms (31.24%) | Memory: 59.3 MB (32.83%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -16,19 +16,19 @@ class Solution {
                 int sum=nums[i]+nums[j]+nums[k];
                 if(sum==0){
                     res.add(Arrays.asList(nums[i],nums[j],nums[k]));
-                   while(j<k && nums[j]==nums[j+1]){ j++;}
+                  while(j<k && nums[j]==nums[j+1]){ j++;}
                        while(j<k && nums[k]==nums[k-1]){ k--;}
                     j++;
                     k--;
-                   
+                    
                 }
                  
                 else if(sum>0){
-                     while(j<k && nums[k]==nums[k-1]){ k--;}
+                     
                      k--;
                 }
                 else{
-                while(j<k && nums[j]==nums[j+1]){ j++;}
+              
                     j++;
                 }
             }
