32aab059ab2fd6f2ec6738d1f5b3c36ab320068b
Dolisha Arora <dolisha1091.becse24@chitkara.edu.in>
2026-07-14 17:28:28 +0530

    Time: 2 ms (100.00%) | Memory: 88.3 MB (6.10%) - LeetSync

---
+++ 2394-count-subarrays-with-score-less-than-k/count-subarrays-with-score-less-than-k.java
@@ -0,0 +1,19 @@
+class Solution {
+    public long countSubarrays(int[] nums, long k) {
+        long count=0;
+        int left=0;
+        long sum=0;
+        int right=0;
+        while(right<nums.length){
+            sum+=nums[right];
+            while(sum*(right-left+1)>=k){
+                sum=sum-nums[left];
+                left++;
+
+            }
+            count+=right-left+1;
+            right++;
+        }
+        return count;
+    }
+}
