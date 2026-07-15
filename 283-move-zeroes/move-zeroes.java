5a34be99c8706cef6bfc79449a234a64f4822e06
Dolisha Arora <dolisha1091.becse24@chitkara.edu.in>
2026-07-15 13:14:30 +0530

    Time: 3 ms (19.02%) | Memory: 47.7 MB (57.13%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -2,27 +2,23 @@ class Solution {
     public void moveZeroes(int[] nums) {
         int n=nums.length;
         int left=0;
-        int right=left+1;
-        while(right<n && left<right){
-         if(nums[left]!=0){
-                left++;
-                right++;
+        int right=1;
+        while(right<n){
+            if(nums[left]!=0 ) {
+                left++; right++;
             }
-           else if(nums[left]==0 && nums[right]!=0){
-                //swap
+            else if(nums[left]==0 && nums[right]!=0){
                 int temp=nums[left];
                 nums[left]=nums[right];
                 nums[right]=temp;
                 left++;
                 right++;
             }
-            else if(nums[left]==0 && nums[right]==0){
+            else{
                 right++;
             }
-            
         }
-        
-        
+
         
     }
-} 
+}
