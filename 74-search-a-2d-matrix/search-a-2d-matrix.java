84fb4bca5f8230a6ade16fdbf7075f3c109eb4e2
Dolisha Arora <dolisha1091.becse24@chitkara.edu.in>
2026-07-17 12:07:43 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (24.68%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -2,7 +2,7 @@ class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         int n=matrix.length;
         for(int i=0;i<n;i++){
-            for(int j=0;j<matrix[0].length;j++){
+            for(int j=0;j<matrix[i].length;j++){
                 if(matrix[i][j]==target){
                     return true;
                 }
