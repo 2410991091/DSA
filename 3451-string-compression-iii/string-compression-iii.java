e45bda0bb266fe6159228c53018755de4c37ac92
Dolisha Arora <dolisha1091.becse24@chitkara.edu.in>
2026-08-06 17:20:32 +0530

    Time: 2076 ms (5.16%) | Memory: 54 MB (13.84%) - LeetSync

---
+++ 3451-string-compression-iii/string-compression-iii.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String compressedString(String word) {
+        int n=word.length();
+        String ans="";
+        for(int i=0;i<n;i++){
+            int count=1;
+            char ch=word.charAt(i);
+            while(i<n-1 && ch==word.charAt(i+1)){
+                count++;
+                i++;
+
+                if(count==9){
+                    break;
+                }
+             
+            }
+            // if(count>=10){
+
+            //     String s=word.substring(i,n-1);
+
+            //     return compressedString(s);
+            // }
+            ans+=Integer.valueOf(count);
+            ans+=ch;
+
+        }
+        return ans;
+    }
+}
