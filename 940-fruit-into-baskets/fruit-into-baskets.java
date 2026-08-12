765f8e85c280a291f65cd8061ad2f063d4c61800
Dolisha Arora <dolisha1091.becse24@chitkara.edu.in>
2026-08-12 12:00:37 +0530

    Time: 54 ms (66.57%) | Memory: 70.5 MB (56.88%) - LeetSync

---
+++ 940-fruit-into-baskets/fruit-into-baskets.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int totalFruit(int[] fruits) {
+        int k=2;
+        HashMap<Integer,Integer> map=new HashMap<>();
+        int left=0;
+        int right=0;
+        int max=0;
+        while(right<fruits.length){
+            map.put(fruits[right],map.getOrDefault(fruits[right],0)+1);
+            while(map.size()>k){
+               map.put(fruits[left],map.get(fruits[left])-1);
+               if(map.get(fruits[left])==0){
+                map.remove(fruits[left]);
+               }
+              
+                left++;
+                 
+            }
+            
+                max=Math.max(max,right-left+1);
+            right++;
+        }
+  return max;      
+    }
+}
