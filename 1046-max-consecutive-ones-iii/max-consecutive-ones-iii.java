81bafc9e5465edb22185569f5b2dbac15f4b2d30
Dolisha Arora <dolisha1091.becse24@chitkara.edu.in>
2026-08-12 13:10:44 +0530

    Time: 3 ms (95.77%) | Memory: 51.6 MB (98.93%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
@@ -1,31 +1,35 @@
 class Solution {
     public int longestOnes(int[] nums, int k) {
+
         int n = nums.length;
         int left = 0;
         int right = 0;
-        int zerocount = 0;
-        int max = 0;
+        int total = 0;
+        int track = 0;
 
         while (right < n) {
 
-            // Current element enters the window
-            if (nums[right] == 0)
-                zerocount++;
+            // Agar current element 0 hai
+            if (nums[right] == 0) {
+                track++;
+            }
 
-            // Shrink window until it becomes valid
-            while (zerocount > k) {
-                if (nums[left] == 0)
-                    zerocount--;
+            // Agar zeros k se zyada ho gaye
+            while (track > k) {
+
+                if (nums[left] == 0) {
+                    track--;
+                }
+
                 left++;
             }
 
-            // Update answer
-            max = Math.max(max, right - left + 1);
+            // Current valid window ki maximum length
+            total = Math.max(total, right - left + 1);
 
-            // Expand window
             right++;
         }
 
-        return max;
+        return total;
     }
 }
