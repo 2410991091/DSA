c6c8c49da7161b41cc40d313ab57d48635f4695d
Dolisha Arora <dolisha1091.becse24@chitkara.edu.in>
2026-08-25 11:32:25 +0530

    Time: 1 ms (82.00%) | Memory: 44.4 MB (99.08%) - LeetSync

--- 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.java
+++ 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.java
@@ -1,8 +1,6 @@
 class Solution {
     public int missingMultiple(int[] nums, int k) {
-
         for (int i = 1; ; i++) {
-
             int a = k * i;
             boolean found = false;
 
@@ -19,6 +17,6 @@ class Solution {
             }
         }
 
-        
+    
     }
 }
