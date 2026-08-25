72ca142dbb1de48c6d2a2f3c05db5d4ea4bcd411
Dolisha Arora <dolisha1091.becse24@chitkara.edu.in>
2026-08-25 11:17:39 +0530

    Time: 0 ms (100.00%) | Memory: 44.5 MB (98.55%) - LeetSync

---
+++ 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int missingMultiple(int[] nums, int k) {
+
+        for (int i = 1; ; i++) {
+
+            int a = k * i;
+            boolean found = false;
+
+            for (int j = 0; j < nums.length; j++) {
+
+                if (nums[j] == a) {
+                    found = true;
+                    break;
+                }
+            }
+
+            if (!found) {
+                return a;
+            }
+        }
+
+        
+    }
+}
