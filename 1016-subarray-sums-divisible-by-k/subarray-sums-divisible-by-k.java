cf5e712e0a930d70907e9fa1cda034d83b1df0a1
Dolisha Arora <dolisha1091.becse24@chitkara.edu.in>
2026-09-09 13:06:44 +0530

    Time: 25 ms (56.25%) | Memory: 51.5 MB (55.59%) - LeetSync

--- 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.java
+++ 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.java
@@ -1,9 +1,9 @@
 class Solution {
     public int subarraysDivByK(int[] nums, int k) {
-        int count=0;
+        int n=nums.length;
         HashMap<Integer,Integer> map=new HashMap<>();
         map.put(0,1);
-        int n=nums.length;
+        int count=0;
         int sum=0;
         for(int i=0;i<n;i++){
             sum+=nums[i];
@@ -15,7 +15,8 @@ class Solution {
                 count+=map.get(rem);
             }
             map.put(rem,map.getOrDefault(rem,0)+1);
-        }
-        return count;
+
+        }   
+        return count;   
     }
 }
