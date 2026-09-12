1d52dd44d26209ec2dd8f2066ad972523d93d3c6
Dolisha Arora <dolisha1091.becse24@chitkara.edu.in>
2026-09-12 14:01:47 +0530

    Time: 15 ms (54.95%) | Memory: 47.5 MB (66.69%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,22 +1,33 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-
-        List<Integer> ans = new ArrayList<>();
-        HashMap<Integer, Integer> map = new HashMap<>();
-
-        for (int i = 0; i < nums.length; i++) {
-            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        //int[] ans=new int[k];
+        int n=nums.length;
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<n;i++){
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
         }
+      PriorityQueue<Integer> pq = new PriorityQueue<>(
+    (a,b) -> map.get(a) - map.get(b)
+);
 
-        for (int key : map.keySet()) {
-            ans.add(key);
-        }
+// top k
+for(int num : map.keySet()) {
+    pq.add(num);
 
-        ans.sort((a, b) -> map.get(b) - map.get(a));
+    if(pq.size() > k) {
+        pq.poll();
+    }
+}
 
-        return ans.stream()
-                  .limit(k)
-                  .mapToInt(Integer::intValue)
-                  .toArray();
+// answer
+int[] ans = new int[k];
+
+int i = 0;
+
+while(!pq.isEmpty()) {
+    ans[i++] = pq.poll();
+}
+
+return ans;
     }
 }
