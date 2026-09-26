cb4eddb8867f212da9fa692cf6ed79d8cd148f76
Dolisha Arora <dolisha1091.becse24@chitkara.edu.in>
2026-09-26 18:02:27 +0530

    Time: 40 ms (23.36%) | Memory: 48.2 MB (18.36%) - LeetSync

--- 1320-remove-all-adjacent-duplicates-in-string-ii/remove-all-adjacent-duplicates-in-string-ii.java
+++ 1320-remove-all-adjacent-duplicates-in-string-ii/remove-all-adjacent-duplicates-in-string-ii.java
@@ -1,45 +1,37 @@
 class Solution {
     public String removeDuplicates(String s, int k) {
+        int n=s.length();
+        Stack<Character> st1=new Stack<>();
+        Stack<Integer> st2=new Stack<>();
+        for(int i=0;i<n;i++){
+            char ch=s.charAt(i);
 
-        int n = s.length();
-
-        Stack<Character> st1 = new Stack<>();
-        Stack<Integer> st2 = new Stack<>();
-
-        for (int i = 0; i < n; i++) {
-
-            char ch = s.charAt(i);
-
-            if (!st1.isEmpty() && st1.peek() == ch) {
-
-                int x = st2.pop();
-
-                st2.push(x + 1);
-
-                if (x + 1 == k) {
+            if(!st1.isEmpty() && st1.peek()==ch){
+                int x=st2.pop();
+                st2.push(x+1);
+                if(x+1==k){
                     st1.pop();
                     st2.pop();
                 }
 
-            } else {
 
-                st1.push(ch);
-                st2.push(1);
             }
+
+else{
+    st1.push(ch);
+            st2.push(1);
+
+}
         }
-
-        StringBuilder ans = new StringBuilder();
-
-        while (!st1.isEmpty()) {
-
-            char ch = st1.pop();
-            int count = st2.pop();
-
-            for (int i = 0; i < count; i++) {
-                ans.append(ch);
-            }
+     
+     StringBuilder ans=new StringBuilder();
+     while(!st1.isEmpty()){
+        char ch=st1.pop();
+        int count=st2.pop();
+        for(int i=0;i<count;i++){
+            ans.append(ch);
         }
-
-        return ans.reverse().toString();
+     }
+     return ans.reverse().toString();
     }
 }
